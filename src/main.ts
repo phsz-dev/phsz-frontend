0aeb4e892ea1dd5cc429e297ff41ab12aeba3e93
yhzx233 <2058777361@qq.com>
2024-03-08 19:28:12 +0800

    chore: clean up

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,7 @@
 import { createApp } from 'vue'
 import './style.css'
-import './index.css'
 import App from './App.vue'
 import router from './router'
-import 'tailwindcss/tailwind.css'
 
 const app = createApp(App)
 app.use(router)
