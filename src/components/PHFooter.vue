4f017700fb3a26664d08f2df210d6f2a8c7f8088
yhzx233 <2058777361@qq.com>
2024-03-15 19:56:28 +0800

    fix: darkmode transition

--- src/components/PHFooter.vue
+++ src/components/PHFooter.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="absolute bottom-0 max-w-7xl w-full text-center text-xs p-2">
+  <footer class="absolute bottom-0 max-w-7xl w-full text-center text-xs p-2 dark:text-gray-100">
     <p>Released under the MIT License.</p>
     <p>Copyright © 2024-present phze-dev</p>
   </footer>
