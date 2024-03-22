564979a251d0957c2801e6d624ab339f130e6b78
yhzx233 <2058777361@qq.com>
2024-03-22 10:09:54 +0800

    fix: home color

--- src/components/PHHomeHero.vue
+++ src/components/PHHomeHero.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="p-16">
     <h1 class="text-6xl leading-snug">
-      <span class="text-primary">PHSZ</span>
+      <span class="text-primary-500">PHSZ</span>
       <span class="dark:text-gray-100">- 新一代</span>
     </h1>
     <p class="text-6xl font-bold dark:text-gray-100">宠物医院学习系统</p>
