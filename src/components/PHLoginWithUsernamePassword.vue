e222e1fdd698d74b4709f56e72c0f88b493013c2
mofengyichen <3309113612@qq.com>
2024-03-27 01:54:53 +0800

    feat: ugly login page

---
+++ src/components/PHLoginWithUsernamePassword.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="mx-auto max-w-md py-4 flex flex-col">
+        <input type="text" class="placeholder:text-gray-600 placeholder:dark:text-gray-200 outline-none p-4 my-2 bg-transparent border-b-gray-600 border-b-2 dark:border-b-gray-200 text-gray-600 dark:text-gray-200" placeholder="请输入用户名" />
+        <input type="password" class="placeholder:text-gray-600 placeholder:dark:text-gray-200 outline-none p-4 my-2 bg-transparent border-b-2 border-b-gray-600 dark:border-b-gray-200 text-gray-600 dark:text-gray-200" placeholder="请输入密码" />
+        <button class="p-3 w-56 my-8 mx-auto bg-primary-500 text-white rounded-full dark:text-gray-200 dark:!bg-dark-block-600" @click="login">
+            登录
+        </button>
+    </div>
+</template>
+
+<script setup lang="ts">
+    import { useUserStore } from '../stores/user';
+    const userStore = useUserStore();
+    const login = () => {
+        // 模拟登录
+        userStore.login();
+    }
+</script>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,21 +1,10 @@
 <template>
   <div class="mx-auto max-w-7xl">
     <!-- 模拟登录按钮 -->
-    <button class="p-4 bg-primary-500 text-white rounded-lg" @click="login">
-      模拟登录
-    </button>
+    <PHLoginWithUsernamePassword />
   </div>
 </template>
 
 <script setup lang="ts">
-
-import { useUserStore } from '../stores/user';
-
-const userStore = useUserStore();
-
-const login = () => {
-  // 模拟登录
-  userStore.login();
-}
-
+import PHLoginWithUsernamePassword from '../components/PHLoginWithUsernamePassword.vue'
 </script>
