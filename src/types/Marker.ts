3e14ff45e04354371196393c56c0dddec57fe9cc
yhzx233 <2058777361@qq.com>
2024-04-03 14:01:24 +0800

    feat: improve 3d navigation & transition

---
+++ src/types/Marker.ts
@@ -0,0 +1,6 @@
+export default interface Marker {
+  id?: number
+  name?: string
+  description?: string
+  position: [number, number, number]
+}
--- src/views/3DNavigation.vue
+++ src/views/3DNavigation.vue
@@ -1,19 +1,32 @@
 <template>
-  <TresCanvas
-    ref="canvas"
-    window-size
-    :clear-color="dark ? '#000' : '#fff'"
-    @click="onCanvasClick"
-  >
-    <TresPerspectiveCamera :position="[5, 5, 5]" />
-    <OrbitControls />
+  <div class="root">
+    <TresCanvas
+      ref="canvas"
+      window-size
+      :clear-color="dark ? '#000' : '#f4f8ff'"
+      @click="onCanvasClick"
+    >
+      <TresPerspectiveCamera :position="[5, 5, 5]" />
+      <OrbitControls />
 
-    <TresMesh>
-      <TresTorusGeometry :args="[1, 0.5, 16, 32]" />
-      <TresMeshBasicMaterial color="orange" />
-    </TresMesh>
-    <TresGridHelper />
-  </TresCanvas>
+      <TresMesh>
+        <TresTorusGeometry :args="[1, 0.5, 16, 32]" />
+        <TresMeshBasicMaterial color="orange" />
+      </TresMesh>
+      <TresGridHelper />
+    </TresCanvas>
+    <Transition name="fade">
+      <div
+        v-if="selected"
+        class="fixed right-8 top-1/2 h-1/2 w-1/4 -translate-y-1/2 bg-white/75"
+      >
+        <div class="p-4">
+          <h2 class="text-lg font-bold">{{ markers[selected].name }}</h2>
+          <p class="text-sm">{{ markers[selected].description }}</p>
+        </div>
+      </div>
+    </Transition>
+  </div>
 </template>
 
 <script lang="ts" setup>
@@ -25,29 +38,41 @@ import { OrbitControls } from '@tresjs/cientos'
 import * as THREE from 'three'
 import { onMounted, ref } from 'vue'
 
-const markers : Record<string, { position: [number, number, number] }> = {
-  '1': { position: [3, 0, 3] },
-  '2': { position: [-3, 0, -3] },
+import Marker from '../types/Marker'
+
+const markers: Record<string, Marker> = {
+  '1': {
+    id: 1,
+    position: [3, 0, 3],
+    name: '手术室',
+    description: '这是一个手术室'
+  },
+  '2': {
+    id: 2,
+    position: [-3, 0, -3],
+    name: '病房',
+    description: '这是一个病房'
+  }
 }
 
 const markers_group = new THREE.Group()
 
-// 地图标记
-const markerUrl = '/map_marker.svg'
-
 const canvas = ref<any>(null)
-let context : TresContext
+let context: TresContext
 
 onMounted(async () => {
   context = canvas.value.context
   context.scene.value.add(markers_group)
 
+  const markerUrl = '/map_marker.svg'
   const markerTexture = await useTexture([markerUrl])
 
   // 添加地图标记
   for (const key in markers) {
     const marker = markers[key]
-    const sprite = new THREE.Sprite(new THREE.SpriteMaterial({ map: markerTexture }))
+    const sprite = new THREE.Sprite(
+      new THREE.SpriteMaterial({ map: markerTexture })
+    )
     sprite.name = key
     sprite.scale.set(0.4, 0.6, 1)
     sprite.center.set(0.5, 0)
@@ -55,6 +80,8 @@ onMounted(async () => {
     markers_group.add(sprite)
   }
 })
+
+const selected = ref<string | null>(null)
 
 let raycaster = new THREE.Raycaster()
 
@@ -68,7 +95,35 @@ const onCanvasClick = (e: MouseEvent) => {
   if (intersects.length > 0) {
     // 按照距离排序
     intersects.sort((a, b) => a.distance - b.distance)
-    console.log(intersects[0].object.name)
+    selected.value = intersects[0].object.name
+    console.log('selected:', selected.value)
+  } else {
+    selected.value = null
   }
 }
 </script>
+
+<style scoped>
+
+.slide-enter-from,
+.slide-enter-to,
+.slide-leave-to,
+.slide-leave-from {
+  transform: translateY(-64px);
+}
+
+.slide-leave-active {
+  transition: none;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: all 0.3s ease-out;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  transform: translate(100%, -50%);
+  opacity: 0;
+}
+</style>
